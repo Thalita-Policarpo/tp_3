4061b3c1e20866e8271809813f450f3b9d6a810f
Thalita Policarpo <thalita_policarpo@outlook.com>
2022-12-05 04:18:28 -0300

    correção

---
+++ TP_03_ThalitaPolicarpo_Entrega/src/br/edu/infnet/tp03_thalita_policarpo/exceptions/InvalidFillmentException.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.tp03_thalita_policarpo.exceptions;
+
+public class InvalidFillmentException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidFillmentException (String msg) {
+		super(msg);
+	}
+
+}
